aed01b2c4c190ea1d58912b88d6e6c2c25b9a93d
ceciliawambui <ceciliawambui026@gmail.com>
2025-03-03 19:51:00 +0300

    testing

--- blogapp/tests/test_models.py
+++ blogapp/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogapp.models import Post
+# testing
 
 class PostModelTest(TestCase):
     """Tests for the Post model"""
